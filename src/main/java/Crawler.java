bf0e9b65eed8cd3d5374506b0c26b97d5241b9c7
ryderdinh <dinhquanganhdev@gmail.com>
2022-10-02 21:45:25 +0700

    update crawler

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -1,54 +1,74 @@
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Crawler {
-    private static Document request(String url, ArrayList<String> v) {
-        try {
-            Connection con = Jsoup.connect(url);
-            Document doc = con.get();
+    private String title = "";
+    private Document doc;
 
-            if (con.response().statusCode() == 200) {
-                System.out.println("Link: " + url);
-                System.out.println(doc.title());
-                v.add(url);
+    public static void print(String msg, Object... args) {
+        System.out.println(String.format(msg, args));
+    }
 
-                return doc;
-            }
+    private static String trim(String s, int width) {
+        if (s.length() > width)
+            return s.substring(0, width - 1) + ".";
+        else
+            return s;
+    }
 
-            return null;
-        } catch (IOException e) {
-            return null;
+    public Crawler(Document doc) {
+        this.title = doc.title();
+        this.doc = doc;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Document getDoc() {
+        return doc;
+    }
+
+    public void getMedia() {
+        Elements media = doc.select("[src]");
+
+        print("\nMedia: (%d)", media.size());
+        for (Element src : media) {
+            if (src.normalName().equals("img"))
+                print(" * %s: <%s> %sx%s (%s)",
+                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
+                        trim(src.attr("alt"), 20));
+            else
+                print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
         }
     }
 
-    private static void crawl(int level, String url, ArrayList<String> visited) {
-        if (level <= 5) {
-            Document doc = request(url, visited);
+    public void getImports() {
+        Elements imports = doc.select("link[href]");
 
-            if (doc != null) {
-                for (Element link : doc.select(("a[href]"))) {
-                    String next_link = link.absUrl("href");
-                    if (visited.contains(next_link) == false) {
-                        crawl(level++, next_link, visited);
-                    }
-                }
-            }
+        print("\nImports: (%d)", imports.size());
+        for (Element link : imports) {
+            print(" * %s <%s> (%s)", link.tagName(), link.attr("abs:href"), link.attr("rel"));
         }
     }
 
+    public void getLinks() {
+        Elements links = doc.select("a[href]");
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your link: ");
-        String url = sc.nextLine();
-        sc.close();
-
-        crawl(1, url, new ArrayList<String>());
+        print("\nLinks: (%d)", links.size());
+        for (Element link : links) {
+            print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
+        }
     }
+
+    public void getAllData() {
+        print("Title: " + getTitle());
+        getMedia();
+        getImports();
+        getLinks();
+    }
+
+
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void print(Boolean ln, String msg, Object... args) {
+        if (!ln) System.out.print(String.format(msg, args));
+        else System.out.println(String.format(msg, args));
+    }
+
+    public static String enterURL() {
+        print(false, "Enter your link: ");
+        return sc.nextLine();
+    }
+
+    public static Document fetchFromURL(String url) throws IOException {
+        print(true, "Fetching %s...", url);
+        try {
+            Document doc = Jsoup.connect(url).get();
+            return doc;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String url = "";
+        url = enterURL();
+
+        Crawler crw = new Crawler(Objects.requireNonNull(fetchFromURL(url)));
+        crw.getAllData();
+    }
+}
