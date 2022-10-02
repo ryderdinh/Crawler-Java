539ef6b1c414e9a2611a992e46dfa94285f118f2
ryderdinh <dinhquanganhdev@gmail.com>
2022-10-02 21:45:51 +0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.Objects;
